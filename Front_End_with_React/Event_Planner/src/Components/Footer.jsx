a12374504b125c00117286a5f1bff8bf0279154d
Joeladams02 <Joeladams02@icloud.com>
2024-12-28 18:16:42 +0000

    commit

---
+++ Front_End_with_React/Event_Planner/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './EventPlanner.css'; // Import CSS file for styling
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Great practice of Componenent Composition</p>
+        </footer>
+    );
+};
+
+export default Footer;
