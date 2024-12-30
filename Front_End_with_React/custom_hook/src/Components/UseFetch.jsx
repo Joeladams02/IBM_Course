e9e8579414e279b93161e0a32abd92d642dc9d0e
Joeladams02 <Joeladams02@icloud.com>
2024-12-30 22:35:59 +0000

    take 1

--- Front_End_with_React/custom_hook/src/Components/UseFetch.jsx
+++ Front_End_with_React/custom_hook/src/Components/UseFetch.jsx
@@ -1,5 +1,17 @@
+import React, { useState, useEffect } from "react"
+
 const UseFetch = (url) => {
-    
+    //This custom hook will fetch data from the url passed as an argument
+    const [data, setData] = useState(); //data is stored in the state data, we use setData to update the state
+    useEffect(() => {
+        fetch(url) //fetch request is made to the url passed as an argument
+            .then((response) => response.json()) //The response is converted to json
+            .then((data) => setData(data)) //The data fetched is stored in the state data
+    },[]) //The empty array as the second argument of useEffect ensures that the fetch request is only made once when the component mounts
+    return [data]; //The data fetched is returned
 }
 
 export default UseFetch
+
+//This concludes the custom hook UseFetch.
+//We will now use this custom hook in the FetchData component to fetch data from the API.
