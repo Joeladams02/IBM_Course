58413078198bd872b5d38683796bd595890e8f18
Joeladams02 <Joeladams02@icloud.com>
2025-01-05 19:52:09 +0000

    commit

--- Back_End_with_Node/promises/promisescript.js
+++ Back_End_with_Node/promises/promisescript.js
@@ -15,7 +15,34 @@ let myPromise2 = new Promise((resolve, reject) => {
 //However, both promises are run at the same time and then the messages are printed in the order they are resolved.
 myPromise.then((resolveMessage) => {
     console.log(resolveMessage);
-    myPromise2.then((resolveMessage) => {
-        console.log(resolveMessage);
-    });
+    return myPromise2;
 })
+.then((resolveMessage) => {
+        console.log(resolveMessage);
+});
+
+
+//The following code does the same as the above but is written without the need for nested for nested .then calls. 
+//We await the result from both promise1 and promise2. Once they're both completed, they print.
+async function myAsyncFunction() {
+    try {
+        let [result1, result2] = await Promise.all([
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve("Promise 1 is resolved");
+                }, 6000);
+            }),
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve("Promise 2 is resolved");
+                }, 3000);
+            })
+        ]);
+        console.log(result1);
+        console.log(result2);
+    } catch (err) {
+        console.log('error')
+    }
+}
+
+myAsyncFunction();
